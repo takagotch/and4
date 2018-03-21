d113386ff59639336264241a7e86dc19c58ee84d
takagotch <dyaccb@gmail.com>
2018-03-22 07:45:44 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ 3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.username.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle saveInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
+
