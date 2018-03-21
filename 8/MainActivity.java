66f491bff833385000436e61be24e8d94683ad88
takagotch <dyaccb@gmail.com>
2018-03-22 02:33:36 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- 8/MainActivity.java
+++ 8/MainActivity.java
@@ -1,11 +1,19 @@
 public class MainActivity extends AppCompatActivity{
+  
   private Realm mRealm;
+  private ListView mListView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState){
     fab.setOnClickListener(new View.OnClickListener(){
     });
     mRealm = Realm.getDefaultInstance();
+
+    mListView = (ListView) findViewById(R.id.listView);
+    RealmResults<Schedule> schedules
+	    = mRealm.where(Schedule.class).findAll();
+    ScheduleAdapter adapter = new ScheduleAdapter(schedules);
+    mListView.setAdapter(adapter);
   }
 
   @Override
