189383a2a3051d8d891dc2518ab9d0545d4b7fb5
takagotch <dyaccb@gmail.com>
2018-03-22 07:53:01 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- 3/MainActivity.java
+++ 3/MainActivity.java
@@ -9,6 +9,34 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle saveInstanceState){
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Button tapHere = (Button)findViewById(R.id.tapHere);
+    tapHere.setOnClickListener(new View.OnClickListener(){
+      @Override
+      public void onClick(View view){
+        TextView textView = (TextView)findViewById(R.id.textView);
+	textView.setText("BUTTON TAPPED");
+      }
+    });
+
+    Button tapHere = (Button)findViewById(R.id.tapHere);
+    MyOnClickListener myOnClickListener = new MyOnClickListener();
+    tapHere.setOnClickListener(myOnClickListener);
   }
+}
+
+public class MyOnClickListener implements View.OnClickListener{
+  @Override
+  public void onClick(View v){
+    TextView textView = (TextView)findViewById(R.id.textView);
+    textView.setText(R.string.hello_world);
+  }
+}
+
+
+
+public void onClickButton(View view){
+  TextView textView = (TextView)findViewById(R.id.textView);
+  textView.setText("BUTTON TAPPED");
 }
 
